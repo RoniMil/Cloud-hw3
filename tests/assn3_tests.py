55f7d8a73aba3dd462060fa4bcc6a83e784976a0
RoniMil <milsteinroni@gmail.com>
2023-07-14 17:18:51 +0300

    fix testing

--- tests/assn3_tests.py
+++ tests/assn3_tests.py
@@ -48,7 +48,7 @@ def get_dishes_ids(col, dishes):
 # tests for dishes service
 
 def test1():
-    dishes = ["blah", "spaghetti", "apple pie"]
+    dishes = ["orange", "spaghetti", "apple pie"]
     for dish in dishes:
         response = connectionController.http_post("dishes", {"name": dish})
         assert_valid_added_resource(response)
